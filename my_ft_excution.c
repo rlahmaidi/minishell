07184237bfd472e2228803ebc23685c56855f1c4
Rachid Lahmaidi El Idrissi <rlahmaid@e1r5p10.1337.ma>
2021-12-17 14:56:52 +0100

    zidt chi la3ba

--- my_ft_excution.c
+++ my_ft_excution.c
@@ -82,18 +82,53 @@ void ft_excution(t_cmd *strct, t_node *node,char **env)
 	while(strct->next != NULL)
 	{
 		redirection_handler(strct);
-		pipe(fd);
-		pid = fork();
+		int fd_out = open(name, O_CREAT | O_RDONLY | O_TRUNC);
+		int fd_int = open(name, O_CREAT | O_RDONLY | O_TRUNC);
+
+		if(pipe(fd) < 0)
+			return(1);
+		if(pid = fork() < 0)
+			return(1);
 		if(pid == 0)
 		{
+			if(fd_out != -1)
+				dup2(fd_out, 1);
+			if(fd_out == -1)
+				close(fd_out);
+			if()
+				dup2(fd_int, 0);
+			if()
+
 			//pipes should be treated here somehow;
 			if(non_builtin_cmd(strct) && strct->next == NULL)
 			{
 				redirection_handler(strct);//not defined yet;
 				execve(strct->cmd,strct->args,env);//should i search for the path of the command;
+				exit(0);
 			}
 			else
 				builtins(strct,node);
-		}	
+		}
+}
+
+while(cmd)
+{
+	int fd[2];
+	int f = -1;
+	pipe(fd);
+	fork;
+	if(pid == 0)
+	{
+		if(ila makanch how akhir comd)
+			dup2(fd[1],1);
+		if (f != -1)
+			dup2(f, 0);
+		close(fd[0]);
+		exuction;
 	}
+	else
+	[
+		f = fd[0];
+		close(fd[1]);
+	]
 }
